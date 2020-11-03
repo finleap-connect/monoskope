8e131611f6f90bc0fbd7ad32c0c3a5c842956d8b
Jan Steffen <jan.steffen@finleap.com>
2020-11-03 08:15:16 +0100

    Remove unnecessary output

--- cmd/monoctl/main.go
+++ cmd/monoctl/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"gitlab.figo.systems/platform/monoskope/monoskope/pkg/cmd/monoctl"
@@ -9,7 +8,6 @@ import (
 
 func main() {
 	if err := monoctl.NewRootCmd().Execute(); err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 }
