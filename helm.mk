53dba2a06ec0d81dce617a8b223f765b37f66a90
Jan Steffen <jan.steffen@finleap.com>
2020-11-06 15:50:45 +0100

    Helm building via makefile

--- helm.mk
+++ helm.mk
@@ -1,5 +1,7 @@
 HELM                		?= helm3
 HELM_OUTPUT_DIR             ?= tmp
+HELM_REGISTRY 				?= https://artifactory.figo.systems/artifactory/virtual_helm
+
 
 .PHONY: helm-template-clean helm-dependency-update helm-install helm-uninstall helm-template
 
@@ -25,3 +27,12 @@ template-%: clean
 add-kubism:
 	@$(HELM) repo add kubism.io https://kubism.github.io/charts/
 	@$(HELM) repo update
+
+package-%:
+	@cp "$(HELM_PATH)/$*/values.yaml" "$(HELM_PATH)/$*/values.yaml.bkp"
+	@yq write "$(HELM_PATH)/$*/values.yaml" image.tag "$(VERSION)" --inplace
+	@$(HELM) package $(HELM_PATH)/$* --dependency-update --version $(VERSION)
+	@mv "$(HELM_PATH)/$*/values.yaml.bkp" "$(HELM_PATH)/$*/values.yaml"
+
+push-%:
+	@curl --fail -u $(HELM_USER):$(HELM_PASSWORD) -T $*-$(VERSION).tgz "$(HELM_REGISTRY)/$*-$(VERSION).tgz"
