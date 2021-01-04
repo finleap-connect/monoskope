e89f2aa767754f75731e9eddc09072afbd4e13d5
Jan Steffen <jan.steffen@finleap.com>
2021-01-04 16:11:13 +0100

    Fix comment

--- pkg/messaging/suite_test.go
+++ pkg/messaging/suite_test.go
@@ -33,7 +33,7 @@ var _ = BeforeSuite(func(done Done) {
 	err = env.CreateDockerPool()
 	Expect(err).ToNot(HaveOccurred())
 
-	// Start single node crdb
+	// Start rabbitmq
 	container, err := env.RunWithOptions(&dockertest.RunOptions{
 		Name:       "rabbitmq",
 		Repository: "gitlab.figo.systems/platform/dependency_proxy/containers/bitnami/rabbitmq",
@@ -41,21 +41,7 @@ var _ = BeforeSuite(func(done Done) {
 	})
 	Expect(err).ToNot(HaveOccurred())
 
-	// create rabbit conn
-	// rabbitConnectionTry := 1
 	env.amqpURL = fmt.Sprintf("amqp://user:bitnami@%s:%s", "127.0.0.1", container.GetPort("5672/tcp"))
-	// err = env.Retry(func() error {
-	// 	env.Log.Info("Trying to connect rabbitmq...")
-	// 	conn, err := amqp.Dial(env.amqpURL)
-	// 	if err != nil {
-	// 		env.Log.Info(fmt.Sprintf("Failed, retrying in %v seconds ...", rabbitConnectionTry))
-	// 		time.Sleep(time.Duration(rabbitConnectionTry) * time.Second)
-	// 		rabbitConnectionTry++
-	// 		return err
-	// 	}
-	// 	return conn.Close()
-	// })
-	// Expect(err).ToNot(HaveOccurred())
 }, 60)
 
 var _ = AfterSuite(func() {
