35c924de5a949d21f6b7883e8f7e8a942da257a2
Jan Steffen <jan.steffen@finleap.com>
2021-01-06 12:37:38 +0100

    Adjust startup of store

--- cmd/eventstore/server.go
+++ cmd/eventstore/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"os"
 
@@ -12,15 +13,17 @@ import (
 )
 
 var (
-	apiAddr      string
-	metricsAddr  string
-	keepAlive    bool
-	dbAddr       string
-	dbUser       string
-	dbName       string
-	dbPassword   string
-	msgbusUrl    string
-	msgbusPrefix string
+	apiAddr        string
+	metricsAddr    string
+	keepAlive      bool
+	dbAddr         string
+	dbUser         string
+	dbName         string
+	dbPassword     string
+	msgbusPrefix   string
+	msgbusAddr     string
+	msgbusUser     string
+	msgbusPassword string
 )
 
 var serverCmd = &cobra.Command{
@@ -33,6 +36,9 @@ var serverCmd = &cobra.Command{
 		// Some options can be provided by env variables
 		if v := os.Getenv("DB_PASSWORD"); v != "" {
 			dbPassword = v
+		}
+		if v := os.Getenv("BUS_PASSWORD"); v != "" {
+			msgbusPassword = v
 		}
 
 		// Setup grpc listener
@@ -64,6 +70,7 @@ var serverCmd = &cobra.Command{
 		}
 
 		// init message bus publisher
+		msgbusUrl := fmt.Sprintf("amqp://%s:%s@%s", msgbusUser, msgbusPassword, msgbusAddr)
 		publisher, err := messaging.NewRabbitEventBusPublisher(msgbusUrl, "event-store", msgbusPrefix)
 		if err != nil {
 			return err
@@ -92,9 +99,10 @@ func init() {
 	flags.BoolVar(&keepAlive, "keep-alive", false, "If enabled, gRPC will use keepalive and allow long lasting connections")
 	flags.StringVarP(&apiAddr, "api-addr", "a", ":8080", "Address the gRPC service will listen on")
 	flags.StringVar(&metricsAddr, "metrics-addr", ":9102", "Address the metrics http service will listen on")
-	flags.StringVar(&dbAddr, "db-addr", "127.0.0.1:26257", "Store db host:port")
-	flags.StringVar(&dbUser, "db-user", "eventstore", "Store db user")
-	flags.StringVar(&dbName, "db-name", "eventstore", "Store db name")
-	flags.StringVar(&msgbusUrl, "msgbus-url", "amqp://user:bitnami@127.0.0.1:5672", "Messagebus URL")
+	flags.StringVar(&dbAddr, "db-addr", "127.0.0.1:26257", "DB host:port")
+	flags.StringVar(&dbUser, "db-user", "eventstore", "DB user")
+	flags.StringVar(&dbName, "db-name", "eventstore", "DB db name")
+	flags.StringVar(&msgbusAddr, "msgbus-addr", "127.0.0.1:5672", "MessageBus host:port")
+	flags.StringVar(&msgbusUser, "msgbus-user", "eventstore", "MessageBus user")
 	flags.StringVar(&msgbusPrefix, "msgbus-routing-key-prefix", "m8", "Prefix for all messages emitted to the msg bus")
 }
