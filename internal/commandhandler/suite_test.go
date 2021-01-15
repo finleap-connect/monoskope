8da5d3370b319dc6901dc60d4c0ae4d443fcd61c
Jan Steffen <jan.steffen@finleap.com>
2021-01-15 15:36:50 +0100

    Add generic implementation for a grpc server

---
+++ internal/commandhandler/suite_test.go
@@ -0,0 +1,68 @@
+package commandhandler
+
+import (
+	"net"
+	"testing"
+
+	"github.com/onsi/ginkgo/reporters"
+	"google.golang.org/grpc"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	api "gitlab.figo.systems/platform/monoskope/monoskope/pkg/api/commandhandler"
+	"gitlab.figo.systems/platform/monoskope/monoskope/pkg/grpcutil"
+	"gitlab.figo.systems/platform/monoskope/monoskope/pkg/logger"
+)
+
+var (
+	apiListener net.Listener
+	log         logger.Logger
+	grpcServer  *grpcutil.Server
+)
+
+func TestCommandHandler(t *testing.T) {
+	RegisterFailHandler(Fail)
+	junitReporter := reporters.NewJUnitReporter("../../reports/commandhandler-junit.xml")
+	RunSpecsWithDefaultAndCustomReporters(t, "commandhandler/integration", []Reporter{junitReporter})
+}
+
+var _ = BeforeSuite(func(done Done) {
+	defer close(done)
+	var err error
+	log = logger.WithName("TestCommandHandler")
+
+	By("bootstrapping test env")
+
+	// Create server
+	conf := grpcutil.NewServerConfig("command_handler")
+	grpcServer, err = grpcutil.NewServer(conf)
+	Expect(err).ToNot(HaveOccurred())
+
+	commandHandler, err := NewCommandHandler()
+	Expect(err).ToNot(HaveOccurred())
+
+	grpcServer.RegisterService(func(s grpc.ServiceRegistrar) {
+		api.RegisterCommandHandlerServer(s, commandHandler)
+	})
+
+	apiListener, err = net.Listen("tcp", "127.0.0.1:0")
+	Expect(err).ToNot(HaveOccurred())
+
+	// Start server
+	go func() {
+		err := grpcServer.Serve(apiListener, nil)
+		if err != nil {
+			panic(err)
+		}
+	}()
+}, 60)
+
+var _ = AfterSuite(func() {
+	var err error
+	By("tearing down the test environment")
+
+	// Shutdown server
+	grpcServer.Shutdown()
+	err = apiListener.Close()
+	Expect(err).To(BeNil())
+})
---
+++ pkg/grpcutil/server.go
@@ -0,0 +1,138 @@
+package grpcutil
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"gitlab.figo.systems/platform/monoskope/monoskope/pkg/logger"
+	"gitlab.figo.systems/platform/monoskope/monoskope/pkg/metrics"
+	"gitlab.figo.systems/platform/monoskope/monoskope/pkg/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/reflection"
+)
+
+// ServerConfig is the configuration for the API server
+type serverConfig struct {
+	name      string
+	KeepAlive bool
+}
+
+func NewServerConfig(name string) *serverConfig {
+	return &serverConfig{
+		name: name,
+	}
+}
+
+// Server is the implementation of an API Server
+type Server struct {
+	// HTTP-server exposing the metrics
+	http *http.Server
+	// gRPC-server exposing both the API and health
+	grpc *grpc.Server
+	// Logger interface
+	log logger.Logger
+	//
+	shutdown *util.ShutdownWaitGroup
+}
+
+// NewServer returns a new configured instance of Server
+func NewServer(conf *serverConfig) (*Server, error) {
+	s := &Server{
+		http:     metrics.NewServer(),
+		log:      logger.WithName(conf.name),
+		shutdown: util.NewShutdownWaitGroup(),
+	}
+
+	// Configure gRPC server
+	opts := []grpc.ServerOption{ // add prometheus metrics interceptors
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			grpc_prometheus.StreamServerInterceptor,
+		)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_prometheus.UnaryServerInterceptor,
+		)),
+	}
+	if conf.KeepAlive {
+		opts = append(opts, grpc.KeepaliveParams(keepalive.ServerParameters{
+			MaxConnectionIdle: 5 * time.Minute,
+			Time:              2 * time.Second,
+		}))
+	}
+	s.grpc = grpc.NewServer(opts...)
+
+	// Add grpc health check service
+	healthpb.RegisterHealthServer(s.grpc, health.NewServer())
+	// Register the metric interceptors with prometheus
+	grpc_prometheus.Register(s.grpc)
+	// Enable reflection API
+	reflection.Register(s.grpc)
+
+	return s, nil
+}
+
+// RegisterService registers your gRPC service implementation with the server
+func (s *Server) RegisterService(f func(grpc.ServiceRegistrar)) {
+	f(s.grpc)
+}
+
+// Serve starts the api listeners of the Server
+func (s *Server) Serve(apiLis net.Listener, metricsLis net.Listener) error {
+	shutdown := s.shutdown
+
+	if metricsLis != nil {
+		// Start the http server in a different goroutine
+		shutdown.Add(1)
+
+		go func() {
+			s.log.Info("starting to serve prometheus metrics", "addr", metricsLis.Addr())
+			err := s.http.Serve(metricsLis)
+			// If shutdown is expected, we don't care about the error,
+			// but if we do not expect shutdown, we panic!
+			if !shutdown.IsExpected() && err != nil {
+				panic(fmt.Sprintf("shutdown unexpected: %v", err))
+			}
+			s.log.Info("http server stopped")
+			shutdown.Done() // Notify workgroup
+		}()
+	}
+
+	// Start routine waiting for signals
+	shutdown.RegisterSignalHandler(func() {
+		// Stop the HTTP server
+		s.log.Info("http server shutting down")
+		if err := s.http.Shutdown(context.Background()); err != nil {
+			s.log.Error(err, "http server shutdown problem")
+		}
+
+		// And the gRPC server
+		s.log.Info("grpc server stopping gracefully")
+		s.grpc.GracefulStop()
+	})
+
+	s.log.Info("starting to serve grpc", "addr", apiLis.Addr())
+	err := s.grpc.Serve(apiLis)
+	s.log.Info("grpc server stopped")
+
+	// Check if we are expecting shutdown
+	if !shutdown.IsExpected() {
+		panic(fmt.Sprintf("shutdown unexpected, grpc serve returned: %v", err))
+	}
+	// Wait for both shutdown signals and close the channel
+	if ok := shutdown.WaitOrTimeout(30 * time.Second); !ok {
+		panic("shutting down gracefully exceeded 30 seconds")
+	}
+	return err // Return the error, if grpc stopped gracefully there is no error
+}
+
+// Tell the server to shutdown
+func (s *Server) Shutdown() {
+	s.shutdown.Expect()
+}
