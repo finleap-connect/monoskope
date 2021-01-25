77dfc9edf2391609a80cd7231306ab8383c9d6a5
Jan Steffen <jan.steffen@finleap.com>
2021-01-25 16:01:44 +0100

    Remove shortcode

--- cmd/commandhandler/server.go
+++ cmd/commandhandler/server.go
@@ -79,5 +79,5 @@ func init() {
 	flags.BoolVar(&keepAlive, "keep-alive", false, "If enabled, gRPC will use keepalive and allow long lasting connections")
 	flags.StringVarP(&apiAddr, "api-addr", "a", ":8080", "Address the gRPC service will listen on")
 	flags.StringVar(&metricsAddr, "metrics-addr", ":9102", "Address the metrics http service will listen on")
-	flags.StringVarP(&eventStoreAddr, "event-store-api-addr", "a", ":8081", "Address the eventstore gRPC service is listening on")
+	flags.StringVar(&eventStoreAddr, "event-store-api-addr", ":8081", "Address the eventstore gRPC service is listening on")
 }
