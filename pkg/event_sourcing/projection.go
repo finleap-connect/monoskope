21c66dcd840017b81d57a0c0fa1a9d9fa39f623c
Jan Steffen <jan.steffen@finleap.com>
2021-02-02 08:24:30 +0100

    Add comments

--- pkg/event_sourcing/projection.go
+++ pkg/event_sourcing/projection.go
@@ -1,14 +1,21 @@
 package event_sourcing
 
 import (
+	"context"
+
 	"github.com/google/uuid"
 )
 
+// Projection is the interface for projections.
 type Projection interface {
 	// ID returns the ID of the projection.
 	ID() uuid.UUID
 }
 
+// Projector is the interface for projectors.
 type Projector interface {
+	// Matchers returns the matchers for events the implementation handles.
 	Matchers() []*EventMatcher
+	// Project updates the state of the projection occording to the given event.
+	Project(context.Context, Event, Projection) (Projection, error)
 }
