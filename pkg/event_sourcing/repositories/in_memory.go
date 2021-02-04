26119bc50efb21841b24167083deb7ca2a5fbc8d
Jan Steffen <jan.steffen@finleap.com>
2021-02-04 13:31:43 +0100

    Add locking to repo

--- pkg/event_sourcing/repositories/in_memory.go
+++ pkg/event_sourcing/repositories/in_memory.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/google/uuid"
 	es "gitlab.figo.systems/platform/monoskope/monoskope/pkg/event_sourcing"
@@ -11,6 +12,7 @@ import (
 // inMemoryRepository is a repository which stores projections in memory.
 type inMemoryRepository struct {
 	store map[uuid.UUID]es.Projection
+	mutex sync.RWMutex
 }
 
 // NewInMemoryRepository creates a new repository which stores projections in memory.
@@ -22,6 +24,9 @@ func NewInMemoryRepository() es.Repository {
 
 // ById returns a projection for an ID.
 func (r *inMemoryRepository) ById(ctx context.Context, id uuid.UUID) (es.Projection, error) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+
 	if val, ok := r.store[id]; ok {
 		return val, nil
 	}
@@ -30,6 +35,9 @@ func (r *inMemoryRepository) ById(ctx context.Context, id uuid.UUID) (es.Project
 
 // All returns all projections in the repository.
 func (r *inMemoryRepository) All(context.Context) ([]es.Projection, error) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+
 	all := make([]es.Projection, 0)
 	for _, v := range r.store {
 		all = append(all, v)
@@ -39,12 +47,18 @@ func (r *inMemoryRepository) All(context.Context) ([]es.Projection, error) {
 
 // Upsert saves a projection in the storage or replaces an existing one.
 func (r *inMemoryRepository) Upsert(ctx context.Context, p es.Projection) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
 	r.store[p.ID()] = p
 	return nil
 }
 
 // Remove removes a projection by ID from the storage.
 func (r *inMemoryRepository) Remove(ctx context.Context, id uuid.UUID) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
 	delete(r.store, id)
 	return nil
 }
