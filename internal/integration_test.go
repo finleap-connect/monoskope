e04816bb40347f4624efba4c45e5172573d385e2
Jan Steffen <jan.steffen@finleap.com>
2021-02-18 11:44:33 +0100

    Test query side

--- internal/integration_test.go
+++ internal/integration_test.go
@@ -7,6 +7,8 @@ import (
 	. "github.com/onsi/gomega"
 	ch "gitlab.figo.systems/platform/monoskope/monoskope/internal/commandhandler"
 	est "gitlab.figo.systems/platform/monoskope/monoskope/internal/eventstore"
+	"gitlab.figo.systems/platform/monoskope/monoskope/internal/queryhandler"
+	domainApi "gitlab.figo.systems/platform/monoskope/monoskope/pkg/api/domain"
 	cmdData "gitlab.figo.systems/platform/monoskope/monoskope/pkg/api/domain/commanddata"
 	es "gitlab.figo.systems/platform/monoskope/monoskope/pkg/api/eventsourcing"
 	cmd "gitlab.figo.systems/platform/monoskope/monoskope/pkg/domain/commands"
@@ -43,6 +45,13 @@ var _ = Describe("integration", func() {
 		return esClient
 	}
 
+	userServiceClient := func() domainApi.UserServiceClient {
+		addr := testEnv.queryHandlerTestEnv.GetApiAddr()
+		_, client, err := queryhandler.NewUserServiceClient(ctx, addr)
+		Expect(err).ToNot(HaveOccurred())
+		return client
+	}
+
 	It("create a user", func() {
 		command, err := cmd.CreateCommand(commandTypes.CreateUser, &cmdData.CreateUserCommandData{Name: "admin", Email: "admin@monoskope.io"})
 		Expect(err).ToNot(HaveOccurred())
@@ -56,7 +65,13 @@ var _ = Describe("integration", func() {
 		Expect(err).ToNot(HaveOccurred())
 
 		event, err := eventStream.Recv()
-		Expect(err).To(BeNil())
+		Expect(err).ToNot(HaveOccurred())
 		Expect(event).ToNot(BeNil())
+
+		usClient := userServiceClient()
+		user, err := usClient.GetByEmail(ctx, wrapperspb.String("admin@monoskope.io"))
+		Expect(err).ToNot(HaveOccurred())
+		Expect(user).ToNot(BeNil())
+		Expect(user.GetEmail()).To(Equal("admin@monoskope.io"))
 	})
 })
