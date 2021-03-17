092838a980d3db24273cf410619a38cb211a0770
Jan Steffen <jan.steffen@finleap.com>
2021-03-17 16:41:12 +0100

    Validate cert

--- internal/gateway/auth_server.go
+++ internal/gateway/auth_server.go
@@ -2,11 +2,15 @@ package gateway
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
 	"strings"
 	"time"
+
+	"crypto/x509"
+	"encoding/pem"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -17,12 +21,16 @@ import (
 )
 
 const (
-	defaultAuthorizationHeader = "Authorization"
+	HeaderAuthorization = "Authorization"
 
 	HeaderAuthName            = "x-auth-name"
 	HeaderAuthEmail           = "x-auth-email"
 	HeaderAuthIssuer          = "x-auth-issuer"
 	HeaderForwardedClientCert = "x-forwarded-client-cert"
+)
+
+var (
+	ErrTokenMustNotBeEmpty = errors.New("token must not be empty")
 )
 
 // authServer is the AuthN/AuthZ decision API used as Ambassador Auth Service.
@@ -102,6 +110,7 @@ func (s *authServer) registerViews(r *gin.Engine) {
 // auth serves as handler for the auth route of the server.
 func (s *authServer) auth(c *gin.Context) {
 	route := c.Param("route")
+	s.log.Info("Authenticating request...", "route", route)
 
 	if util.GetOperationMode() == util.DEVELOPMENT {
 		// Print headers
@@ -110,16 +119,56 @@ func (s *authServer) auth(c *gin.Context) {
 		}
 	}
 
-	s.log.Info("Token validation requested...", "Route", route)
 	var claims *auth.Claims
-	var err error
-	if claims, err = s.authHandler.Authorize(c.Request.Context(), defaultBearerTokenFromRequest(c.Request)); err != nil {
-		s.log.Error(err, "Token validation failed.")
-		c.String(http.StatusUnauthorized, fmt.Sprintf("error validating token: %v", err.Error()))
-		return
+	if claims = s.tokenValidation(c); claims != nil {
+		s.writeSuccess(c, claims)
 	}
-	s.log.Info("Token validation successful.", "Route", route, "User", claims.Email)
+	if claims = s.certValidation(c); claims != nil {
+		s.writeSuccess(c, claims)
+	}
+	c.String(http.StatusUnauthorized, "authorization failed")
+}
 
+// tokenValidation validates the token provided within the authorization
+func (s *authServer) tokenValidation(c *gin.Context) *auth.Claims {
+	s.log.Info("Validating token...")
+
+	token := defaultBearerTokenFromRequest(c.Request)
+	if token != "" {
+		return nil
+	}
+
+	if claims, err := s.authHandler.Authorize(c.Request.Context(), token); err != nil {
+		s.log.Error(err, "Token validation failed.")
+		return nil
+	} else {
+		s.log.Info("Token validation successful.", "User", claims.Email)
+		return claims
+	}
+}
+
+// tokenValidation validates the client certificate provided within the forwareded client secret header
+func (s *authServer) certValidation(c *gin.Context) *auth.Claims {
+	s.log.Info("Validating client certificate...")
+
+	cert, err := clientCertificateFromRequest(c.Request)
+	if err != nil {
+		s.log.Error(err, "Certificate validation failed.")
+		return nil
+	}
+
+	claims := &auth.Claims{
+		Name:  cert.Subject.CommonName,
+		Email: cert.Subject.CommonName + "@monoskope.io",
+	}
+	s.log.Info("Client certificate validation successful.", "User", claims.Email)
+
+	return claims
+}
+
+// writeSuccess writes all request headers back to the response along with information got from the upstream IdP and sends an http status ok
+func (s *authServer) writeSuccess(c *gin.Context, claims *auth.Claims) {
+	// Copy request headers to response
 	for k := range c.Request.Header {
 		// Avoid copying the original Content-Length header from the client
 		if strings.ToLower(k) == "content-length" {
@@ -138,10 +187,25 @@ func (s *authServer) auth(c *gin.Context) {
 
 // defaultBearerTokenFromRequest extracts the token from header
 func defaultBearerTokenFromRequest(r *http.Request) string {
-	token := r.Header.Get(defaultAuthorizationHeader)
+	token := r.Header.Get(HeaderAuthorization)
 	split := strings.SplitN(token, " ", 2)
 	if len(split) != 2 || !strings.EqualFold(strings.ToLower(split[0]), "bearer") {
 		return ""
 	}
 	return split[1]
+}
+
+// clientCertificateFromRequest extracts the client certificate from header
+func clientCertificateFromRequest(r *http.Request) (*x509.Certificate, error) {
+	pemData := r.Header.Get(HeaderForwardedClientCert)
+	if pemData == "" {
+		return nil, nil
+	}
+
+	block, _ := pem.Decode([]byte(pemData))
+	if block == nil {
+		return nil, nil
+	}
+
+	return x509.ParseCertificate(block.Bytes)
 }
