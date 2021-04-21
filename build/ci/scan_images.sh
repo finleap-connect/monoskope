008a7aa6bd441c24896e0de11b6431b3457aa8e4
Jan Steffen <jan.steffen@finleap.com>
2021-04-21 15:02:36 +0200

    Adjust scanning

--- build/ci/scan_images.sh
+++ build/ci/scan_images.sh
@@ -12,7 +12,7 @@ echo "Checking values files in '$CI_PROJECT_DIR/build/package/helm'..."
 for file in $(find $CI_PROJECT_DIR/build/package/helm -type f -name values.yaml); do
     CURRENT_IMAGE=$(grep "repository:" $file | cut -d':' -f2- | tr -d '[:space:]' | cut -d':' -f3)
     echo "Checking '$CURRENT_IMAGE' ..."
-    if [[ $CURRENT_IMAGE == $CI_REGISTRY* ]]; then
+    if [[ $CURRENT_IMAGE == *"$CI_PROJECT_PATH"* ]]; then
         echo "Scanning '$CURRENT_IMAGE' ..."
         ./roxctl image scan -e $ROX_CENTRAL_API_ENDPOINT --force --image $CURRENT_IMAGE:$VERSION
         ./roxctl image check -e $ROX_CENTRAL_API_ENDPOINT --image $CURRENT_IMAGE:$VERSION
