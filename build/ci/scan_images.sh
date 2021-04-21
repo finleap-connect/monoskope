caad4c8c8c8d688044dd517f68b76fe9abb6f79e
Jan Steffen <jan.steffen@finleap.com>
2021-04-21 15:26:53 +0200

    Fix

--- build/ci/scan_images.sh
+++ build/ci/scan_images.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-echo "Env:"
-echo "CI_PROJECT_PATH=$CI_PROJECT_PATH"
-echo "CI_REGISTRY=$CI_REGISTRY"
-
 echo "Downloading roxctl..."
 curl -k -L -H "Authorization: Bearer $ROX_API_TOKEN" https://$ROX_CENTRAL_ENDPOINT/api/cli/download/roxctl-linux --output ./roxctl
 chmod a+x ./roxctl
@@ -15,7 +11,7 @@ echo "Checking values files in '$CI_PROJECT_DIR/build/package/helm'..."
 
 for file in $(find $CI_PROJECT_DIR/build/package/helm -type f -name values.yaml); do
     CURRENT_IMAGE=$(grep "repository:" $file | cut -d':' -f2- | tr -d '[:space:]' | cut -d':' -f3)
-    if [[ $CURRENT_IMAGE == $CI_REGISTRY* ]]; then
+    if [[ $CURRENT_IMAGE == ${CI_REGISTRY}* ]]; then
         echo "Scanning '$CURRENT_IMAGE' ..."
         ./roxctl image scan -e $ROX_CENTRAL_API_ENDPOINT --force --image $CURRENT_IMAGE:$VERSION
         ./roxctl image check -e $ROX_CENTRAL_API_ENDPOINT --image $CURRENT_IMAGE:$VERSION
