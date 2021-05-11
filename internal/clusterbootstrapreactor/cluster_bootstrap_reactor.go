190652580523324c4fcde920264cfdf572233fa3
Jan Steffen <jan.steffen@finleap.com>
2021-05-11 17:03:09 +0200

    Add event and some logic

--- internal/clusterbootstrapreactor/cluster_bootstrap_reactor.go
+++ internal/clusterbootstrapreactor/cluster_bootstrap_reactor.go
@@ -2,6 +2,7 @@ package clusterboostrap
 
 import (
 	"context"
+	"time"
 
 	"gitlab.figo.systems/platform/monoskope/monoskope/pkg/domain/constants/events"
 	es "gitlab.figo.systems/platform/monoskope/monoskope/pkg/eventsourcing"
@@ -17,9 +18,28 @@ func NewAggregateHandler() es.Reactor {
 
 // HandleEvent handles a given event returns 0..* Events in reaction or an error
 func (r *clusterBootstrapReactor) HandleEvent(ctx context.Context, event es.Event) ([]es.Event, error) {
+	eventsToEmit := make([]es.Event, 0)
+	version := event.AggregateVersion()
+
 	switch event.EventType() {
 	case events.ClusterCreated:
+		version++
+
 		//TODO: Create new shiny bootstrap token
+		token := "12345"
+
+		//TODO: Create proper real event data
+		eventData := []byte(token)
+
+		eventsToEmit = append(eventsToEmit, es.NewEvent(
+			ctx,
+			events.ClusterBootstrapTokenCreated,
+			eventData,
+			time.Now().UTC(),
+			event.AggregateType(),
+			event.AggregateID(),
+			version))
 	}
-	panic("not implemented")
+
+	return eventsToEmit, nil
 }
