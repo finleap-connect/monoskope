bb3840ce1f9b602e96121e466212eaf46c5f15d2
Jan Steffen <jan.steffen@finleap.com>
2021-06-18 08:59:49 +0200

    Add backoff

--- pkg/eventsourcing/eventhandler/reactor_handler.go
+++ pkg/eventsourcing/eventhandler/reactor_handler.go
@@ -2,16 +2,20 @@ package eventhandler
 
 import (
 	"context"
+	"sync"
+	"time"
 
+	"github.com/cenkalti/backoff"
 	apiEs "gitlab.figo.systems/platform/monoskope/monoskope/pkg/api/eventsourcing"
 	es "gitlab.figo.systems/platform/monoskope/monoskope/pkg/eventsourcing"
 	"gitlab.figo.systems/platform/monoskope/monoskope/pkg/logger"
 )
 
 type reactorEventHandler struct {
-	log      logger.Logger
-	esClient apiEs.EventStoreClient
-	reactor  es.Reactor
+	log       logger.Logger
+	esClient  apiEs.EventStoreClient
+	reactor   es.Reactor
+	waitGroup sync.WaitGroup
 }
 
 // NewReactorEventHandler creates an EventHandler which automates storing Events in the EventStore when a Reactor has emitted any.
@@ -25,20 +29,31 @@ func NewReactorEventHandler(esClient apiEs.EventStoreClient, reactor es.Reactor)
 
 // HandleEvent implements the HandleEvent method of the es.EventHandler interface.
 func (m *reactorEventHandler) HandleEvent(ctx context.Context, event es.Event) error {
+	m.waitGroup.Add(1)
 	eventsChannel := make(chan es.Event)
 	go m.handle(ctx, eventsChannel)
 	return m.reactor.HandleEvent(ctx, event, eventsChannel)
 }
 
+func (m *reactorEventHandler) Stop() {
+	m.waitGroup.Wait()
+}
+
 func (m *reactorEventHandler) handle(ctx context.Context, events <-chan es.Event) {
+	defer m.waitGroup.Done()
 	for ev := range events { // Read events from channel
-		for {
-			err := m.storeEvent(ctx, ev)
-			if err != nil {
-				m.log.Error(err, "Failed to send event to EventStore. Retrying...")
-			} else {
-				break
+		params := backoff.NewExponentialBackOff()
+		params.MaxElapsedTime = 60 * time.Second
+		err := backoff.Retry(func() error {
+			if err := m.storeEvent(ctx, ev); err != nil {
+				m.log.Error(err, "Failed to send event to EventStore. Retrying...", "AggregateID", ev.AggregateID(), "AggregateType", ev.AggregateType(), "EventType", ev.EventType())
+				return err
 			}
+			return nil
+		}, params)
+
+		if err != nil {
+			m.log.Error(err, "Failed to send event to EventStore")
 		}
 	}
 }
