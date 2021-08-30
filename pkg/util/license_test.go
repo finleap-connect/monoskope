65c1f45b8ed449bda813f9ca53f14829e1211004
Jan Steffen <jan.steffen@finleap.com>
2021-08-30 09:34:21 +0200

    Fix check

--- pkg/util/license_test.go
+++ pkg/util/license_test.go
@@ -1,15 +1,16 @@
-/*
-Copyright 2020 Backup Operator Authors
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2021 Monoskope Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package util
 
@@ -19,18 +20,23 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var skip = map[string]bool{}
+var skipPaths = []string{
+	"../../pkg/api/",
+}
 
 var _ = Describe("License", func() {
 	It("should exist in every .go-file", func() {
 		err := filepath.Walk("../..", func(path string, fi os.FileInfo, err error) error {
-			if skip[path] {
-				return nil
+			for _, skipPath := range skipPaths {
+				if strings.HasPrefix(path, skipPath) {
+					return nil
+				}
 			}
 			if err != nil {
 				return err
@@ -42,7 +48,7 @@ var _ = Describe("License", func() {
 			if err != nil {
 				return nil
 			}
-			if !bytes.HasPrefix(content, []byte("/*")) {
+			if !bytes.HasPrefix(content, []byte("//")) {
 				return fmt.Errorf("%s: license header missing", path)
 			}
 			return nil
