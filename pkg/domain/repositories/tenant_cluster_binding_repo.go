840d79a829451264d1a58a29c39654e026948488
Jan Steffen <jan.steffen@finleap.com>
2021-10-28 10:51:41 +0200

    Add repo, setup handler
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

---
+++ pkg/domain/repositories/tenant_cluster_binding_repo.go
@@ -0,0 +1,71 @@
+// Copyright 2021 Monoskope Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package repositories
+
+import (
+	"context"
+
+	projections "github.com/finleap-connect/monoskope/pkg/domain/projections"
+	es "github.com/finleap-connect/monoskope/pkg/eventsourcing"
+	esErrors "github.com/finleap-connect/monoskope/pkg/eventsourcing/errors"
+)
+
+type tenantClusterBindingRepository struct {
+	es.Repository
+}
+
+// TenantClusterBindingRepository is a repository for reading and writing tenantclusterbinding projections.
+type TenantClusterBindingRepository interface {
+	es.Repository
+	ReadOnlyTenantClusterBindingRepository
+	WriteOnlyTenantClusterBindingRepository
+}
+
+// ReadOnlyTenantClusterBindingRepository is a repository for reading tenantclusterbinding projections.
+type ReadOnlyTenantClusterBindingRepository interface {
+	// GetAll searches for the a TenantClusterBinding projections.
+	GetAll(context.Context, bool) ([]*projections.TenantClusterBinding, error)
+}
+
+// WriteOnlyTenantClusterBindingRepository is a repository for writing tenantclusterbinding projections.
+type WriteOnlyTenantClusterBindingRepository interface {
+}
+
+// NewTenantClusterBindingRepository creates a repository for reading and writing tenantclusterbinding projections.
+func NewTenantClusterBindingRepository(repository es.Repository) TenantClusterBindingRepository {
+	return &tenantClusterBindingRepository{
+		Repository: repository,
+	}
+}
+
+// GetAll searches for the a TenantClusterBinding projections.
+func (r *tenantClusterBindingRepository) GetAll(ctx context.Context, includeDeleted bool) ([]*projections.TenantClusterBinding, error) {
+	ps, err := r.All(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var tenants []*projections.TenantClusterBinding
+	for _, p := range ps {
+		if t, ok := p.(*projections.TenantClusterBinding); ok {
+			if !t.GetDeleted().IsValid() || includeDeleted {
+				tenants = append(tenants, t)
+			}
+		} else {
+			return nil, esErrors.ErrInvalidProjectionType
+		}
+	}
+	return tenants, nil
+}
