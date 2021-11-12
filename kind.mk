726e11efb253abc365c92245e2158aa847ecc188
Jan Steffen <jan.steffen@finleap.com>
2021-11-12 14:38:28 +0100

    Adjust kind cluster creation
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- kind.mk
+++ kind.mk
@@ -1,5 +1,3 @@
-.PHONY: template-clean dependency-update install uninstall template docs
-
 KIND_VERSION ?= v1.15.12
 CERT_MANAGER_VERSION ?= v1.1.0
 K8S_CLUSTER_NAME := m8-dev-cluster
@@ -10,8 +8,9 @@ HELM_REGISTRY               ?= https://finleap-connect.github.io/charts
 HELM_REGISTRY_ALIAS         ?= finleap-connect
 HELM_RELEASE                ?= m8
 HELM_VALUES_FILE            ?= examples/01-monoskope-kind-values.yaml
+HELM_OUTPUT_DIR             ?= tmp
 
-export KUBECONFIG=tmp/kind-kubeconfig
+KUBECONFIG=tmp/kind-kubeconfig
 
 ##@ Kind
 
@@ -20,6 +19,12 @@ kind-create: ## create kind cluster
 
 kind-delete: ## destroy kind cluster
 	@kind delete cluster --name ${K8S_CLUSTER_NAME}
+
+kind-helm-repos: ## add & update helm repos necessary
+	@$(HELM) repo add $(HELM_REGISTRY_ALIAS) "$(HELM_REGISTRY)"
+	@$(HELM) repo add jetstack https://charts.jetstack.io
+	@$(HELM) repo add dex https://charts.dexidp.io
+	@$(HELM) repo update
 
 kind-trust-anchor: ## create trust-anchor in kind cluster
 	@echo "Generating trust-anchor for m8 PKI..."
@@ -30,17 +35,19 @@ kind-trust-anchor: ## create trust-anchor in kind cluster
 
 kind-install-certmanager:
 	@echo "Installing cert-manager into kind cluster..."
-	@$(HELM) repo add jetstack https://charts.jetstack.io
-	@$(HELM) repo update
 	@$(HELM) upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version ${CERT_MANAGER_VERSION} --values examples/02-kind-cert-manager-values.yaml
 
 kind-install-dex:
 	@echo "Installing dex into kind cluster..."
-	@$(HELM) repo add dex https://charts.dexidp.io
-	@$(HELM) repo update
 	@$(HELM) upgrade --install dex --wait dex/dex --values examples/03-kind-dex-values.yaml
 
-kind-install-monoskope: kind-create kind-trust-anchor kind-install-certmanager kind-install-dex ## installs monoskope into kind cluster using the latest tag available
-	@$(HELM) repo add $(HELM_REGISTRY_ALIAS) "$(HELM_REGISTRY)"
-	@$(HELM) repo update
+kind-helm-clean: ## clean up templated helm charts
+	@rm -Rf $(HELM_OUTPUT_DIR)
+
+kind-helm-template-monoskope: kind-helm-clean
+	@$(HELM) template $(HELM_RELEASE) $(HELM_REGISTRY_ALIAS)/monoskope --namespace monoskope --version $(LATEST_TAG) --values $(HELM_VALUES_FILE) --output-dir $(HELM_OUTPUT_DIR)
+
+kind-install-monoskope: ## installs monoskope into kind cluster using the latest tag available
 	@$(HELM) upgrade --install $(HELM_RELEASE) $(HELM_REGISTRY_ALIAS)/monoskope --namespace monoskope --create-namespace --version $(LATEST_TAG) --values $(HELM_VALUES_FILE)
+
+kind-setup-monoskope: kind-create kind-trust-anchor kind-helm-repos kind-install-certmanager kind-install-dex kind-install-monoskope
