5beac3a87e3f1175dfd812148526477b4e7793db
Jan Steffen <jan.steffen@finleap.com>
2021-11-22 08:26:25 +0100

    Add ability to query cluster accesses granted (#10)
    * Implement additional API methods on tenant cluster bindings
    * Adjust ambassador defaults
    * Adjust kind cluster creation
    * Adjust defaults to work without vaultoperator
    * Update rabbitmq
    * Adjust makefiles

--- helm.mk
+++ helm.mk
@@ -1,12 +1,10 @@
 HELM                        ?= helm
 HELM_PATH 		            ?= build/package/helm
 HELM_VALUES_FILE            ?= examples/00-monoskope-dev-values.yaml
-HELM_OUTPUT_DIR             ?= tmp
+HELM_OUTPUT_DIR             ?= tmp/helm
 HELM_REGISTRY               ?= https://finleap-connect.github.io/charts
 HELM_REGISTRY_ALIAS         ?= finleap-connect
 HELM_RELEASE                ?= m8
-KUBE_NAMESPACE              ?= platform-monoskope
-YQ							?= yq
 
 .PHONY: template-clean dependency-update install uninstall template docs
 
@@ -21,14 +19,14 @@ helm-dep-%: ## update helm dependencies
 helm-lint-%: ## lint helm chart
 	@$(HELM) lint $(HELM_PATH)/$*
 
-helm-install-%: ## install helm chart from local sources
+helm-install-from-local-%: ## install helm chart from local sources
 	@cat $(HELM_VALUES_FILE) | sed "s/0.0.1-local/$(VERSION)/g" > $(HELM_VALUES_FILE).tag
 	@$(HELM) upgrade --install $(HELM_RELEASE) $(HELM_PATH)/$* --namespace $(KUBE_NAMESPACE) --values $(HELM_VALUES_FILE).tag --skip-crds
 	@rm $(HELM_VALUES_FILE).tag
 
-helm-install-from-repo-%: ## install helm chart from build artifact
+helm-install-from-repo: ## install helm chart from build artifact
 	@$(HELM) repo update
-	@$(HELM) upgrade --install $(HELM_RELEASE) $(HELM_REGISTRY_ALIAS)/$* --namespace $(KUBE_NAMESPACE) --version $(VERSION) --values $(HELM_VALUES_FILE) --skip-crds
+	@$(HELM) upgrade --install $(HELM_RELEASE) $(HELM_REGISTRY_ALIAS)/monoskope --namespace $(KUBE_NAMESPACE) --version $(VERSION) --values $(HELM_VALUES_FILE) --skip-crds
 
 helm-uninstall-%: ## uninstall helm chart
 	@$(HELM) uninstall $(HELM_RELEASE) --namespace $(KUBE_NAMESPACE)
@@ -43,7 +41,7 @@ helm-template-%: helm-clean ## template helm chart
 	@echo
 
 helm-add-finleap: ## add finleap helm chart repo
-	@$(HELM) repo add --username $(HELM_USER) --password $(HELM_PASSWORD) $(HELM_REGISTRY_ALIAS) "$(HELM_REGISTRY)"
+	@$(HELM) repo add $(HELM_REGISTRY_ALIAS) "$(HELM_REGISTRY)"
 
 helm-set-version-all:
 	@find $(HELM_PATH) -name 'Chart.yaml' -exec $(YQ) e --inplace '.version = "$(VERSION)"' {} \;
