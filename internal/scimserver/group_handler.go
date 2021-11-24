c4235d1b400f55e844f168072127c138dd2208f2
Jan Steffen <jan.steffen@finleap.com>
2021-11-24 08:28:34 +0100

    Replace with valid answer
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- internal/scimserver/group_handler.go
+++ internal/scimserver/group_handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/elimity-com/scim"
+	"github.com/elimity-com/scim/errors"
 )
 
 type groupHandler struct {
@@ -16,30 +17,40 @@ func NewGroupHandler() scim.ResourceHandler {
 
 // Create stores given attributes. Returns a resource with the attributes that are stored and a (new) unique identifier.
 func (h *groupHandler) Create(r *http.Request, attributes scim.ResourceAttributes) (scim.Resource, error) {
-	panic("not implemented")
+	return scim.Resource{}, errors.ScimError{
+		Status: http.StatusNotImplemented,
+	}
 }
 
 // Get returns the resource corresponding with the given identifier.
 func (h *groupHandler) Get(r *http.Request, id string) (scim.Resource, error) {
-	panic("not implemented")
+	return scim.Resource{}, errors.ScimError{
+		Status: http.StatusNotImplemented,
+	}
 }
 
 // GetAll returns a paginated list of resources.
 // An empty list of resources will be represented as `null` in the JSON response if `nil` is assigned to the
 // Page.Resources. Otherwise, is an empty slice is assigned, an empty list will be represented as `[]`.
 func (h *groupHandler) GetAll(r *http.Request, params scim.ListRequestParams) (scim.Page, error) {
-	panic("not implemented")
+	return scim.Page{}, errors.ScimError{
+		Status: http.StatusNotImplemented,
+	}
 }
 
 // Replace replaces ALL existing attributes of the resource with given identifier. Given attributes that are empty
 // are to be deleted. Returns a resource with the attributes that are stored.
 func (h *groupHandler) Replace(r *http.Request, id string, attributes scim.ResourceAttributes) (scim.Resource, error) {
-	panic("not implemented")
+	return scim.Resource{}, errors.ScimError{
+		Status: http.StatusNotImplemented,
+	}
 }
 
 // Delete removes the resource with corresponding ID.
 func (h *groupHandler) Delete(r *http.Request, id string) error {
-	panic("not implemented")
+	return errors.ScimError{
+		Status: http.StatusNotImplemented,
+	}
 }
 
 // Patch update one or more attributes of a SCIM resource using a sequence of
@@ -50,5 +61,7 @@ func (h *groupHandler) Delete(r *http.Request, id string) error {
 // 2. the Remove operation should return No Content when the value to be remove is already absent.
 // More information in Section 3.5.2 of RFC 7644: https://tools.ietf.org/html/rfc7644#section-3.5.2
 func (h *groupHandler) Patch(r *http.Request, id string, operations []scim.PatchOperation) (scim.Resource, error) {
-	panic("not implemented")
+	return scim.Resource{}, errors.ScimError{
+		Status: http.StatusNotImplemented,
+	}
 }
