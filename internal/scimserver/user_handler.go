81d3ef64a78d01975885788790958a5011a93329
Jan Steffen <jan.steffen@finleap.com>
2021-11-24 17:15:58 +0100

    Convert user
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- internal/scimserver/user_handler.go
+++ internal/scimserver/user_handler.go
@@ -4,9 +4,12 @@ import (
 	"net/http"
 
 	"github.com/elimity-com/scim"
-	"github.com/elimity-com/scim/errors"
+	scim_errors "github.com/elimity-com/scim/errors"
 	"github.com/finleap-connect/monoskope/pkg/api/domain"
+	"github.com/finleap-connect/monoskope/pkg/api/domain/projections"
 	"github.com/finleap-connect/monoskope/pkg/api/eventsourcing"
+	"github.com/finleap-connect/monoskope/pkg/domain/errors"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 type userHandler struct {
@@ -22,23 +25,34 @@ func NewUserHandler(cmdHandlerClient eventsourcing.CommandHandlerClient, userCli
 
 // Create stores given attributes. Returns a resource with the attributes that are stored and a (new) unique identifier.
 func (h *userHandler) Create(r *http.Request, attributes scim.ResourceAttributes) (scim.Resource, error) {
-	return scim.Resource{}, errors.ScimError{
+	return scim.Resource{}, scim_errors.ScimError{
 		Status: http.StatusNotImplemented,
 	}
 }
 
 // Get returns the resource corresponding with the given identifier.
 func (h *userHandler) Get(r *http.Request, id string) (scim.Resource, error) {
-	return scim.Resource{}, errors.ScimError{
-		Status: http.StatusNotImplemented,
+	user, err := h.userClient.GetById(r.Context(), wrapperspb.String(id))
+	if err != nil {
+		err = errors.TranslateFromGrpcError(err)
+		if err == errors.ErrUserNotFound {
+			return scim.Resource{}, scim_errors.ScimError{
+				Status: http.StatusNotFound,
+			}
+		}
+		return scim.Resource{}, scim_errors.ScimError{
+			Status: http.StatusInternalServerError,
+			Detail: err.Error(),
+		}
 	}
+	return toScimUser(user), nil
 }
 
 // GetAll returns a paginated list of resources.
 // An empty list of resources will be represented as `null` in the JSON response if `nil` is assigned to the
 // Page.Resources. Otherwise, is an empty slice is assigned, an empty list will be represented as `[]`.
 func (h *userHandler) GetAll(r *http.Request, params scim.ListRequestParams) (scim.Page, error) {
-	return scim.Page{}, errors.ScimError{
+	return scim.Page{}, scim_errors.ScimError{
 		Status: http.StatusNotImplemented,
 	}
 }
@@ -46,14 +60,14 @@ func (h *userHandler) GetAll(r *http.Request, params scim.ListRequestParams) (sc
 // Replace replaces ALL existing attributes of the resource with given identifier. Given attributes that are empty
 // are to be deleted. Returns a resource with the attributes that are stored.
 func (h *userHandler) Replace(r *http.Request, id string, attributes scim.ResourceAttributes) (scim.Resource, error) {
-	return scim.Resource{}, errors.ScimError{
+	return scim.Resource{}, scim_errors.ScimError{
 		Status: http.StatusNotImplemented,
 	}
 }
 
 // Delete removes the resource with corresponding ID.
 func (h *userHandler) Delete(r *http.Request, id string) error {
-	return errors.ScimError{
+	return scim_errors.ScimError{
 		Status: http.StatusNotImplemented,
 	}
 }
@@ -66,7 +80,37 @@ func (h *userHandler) Delete(r *http.Request, id string) error {
 // 2. the Remove operation should return No Content when the value to be remove is already absent.
 // More information in Section 3.5.2 of RFC 7644: https://tools.ietf.org/html/rfc7644#section-3.5.2
 func (h *userHandler) Patch(r *http.Request, id string, operations []scim.PatchOperation) (scim.Resource, error) {
-	return scim.Resource{}, errors.ScimError{
+	return scim.Resource{}, scim_errors.ScimError{
 		Status: http.StatusNotImplemented,
 	}
+}
+
+func toScimUser(user *projections.User) scim.Resource {
+	created := user.Metadata.Created.AsTime()
+	lastModified := user.Metadata.LastModified.AsTime()
+	deleted := user.Metadata.Deleted.IsValid()
+	return scim.Resource{
+		ID: user.Id,
+		Meta: scim.Meta{
+			Created:      &created,
+			LastModified: &lastModified,
+		},
+		Attributes: scim.ResourceAttributes{
+			"userName": user.Email,
+			"name": map[string]interface{}{
+				"givenName": user.Name,
+			},
+			"active": !deleted,
+			"emails": []interface{}{
+				map[string]interface{}{
+					"primary": true,
+					"value":   user.Email,
+				},
+			},
+		},
+	}
+}
+
+func fromScimUser(scim.Resource) *projections.User {
+	return new(projections.User)
 }
