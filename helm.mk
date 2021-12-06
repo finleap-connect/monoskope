86505cd34b68ac914e8d63b01e2276d393402508
Jan Steffen <jan.steffen@finleap.com>
2021-12-06 14:52:35 +0100

    Fix ci
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- helm.mk
+++ helm.mk
@@ -13,8 +13,8 @@ HELM_RELEASE                ?= m8
 helm-clean: ## clean up templated helm charts
 	@rm -Rf $(HELM_OUTPUT_DIR)
 
-helm-dep: ## update helm dependencies
-	@$(HELM) dep update $(HELM_PATH)/**
+helm-dep-%: ## update helm dependencies
+	@$(HELM) dep update $(HELM_PATH)/$*
 
 helm-lint: ## lint helm chart
 	@$(HELM) lint $(HELM_PATH)/**
