5163bbb974f319c6b34f5b1c2f4ce3f8a8cedced
Jan Steffen <jan.steffen@finleap.com>
2021-12-10 11:03:40 +0100

    Fix errors
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- pkg/domain/errors/errors.go
+++ pkg/domain/errors/errors.go
@@ -77,6 +77,7 @@ var (
 		},
 		codes.AlreadyExists: {
 			ErrUserAlreadyExists,
+			ErrUserRoleBindingAlreadyExists,
 			ErrTenantAlreadyExists,
 			ErrClusterRegistrationAlreadyExists,
 			ErrClusterAlreadyExists,
