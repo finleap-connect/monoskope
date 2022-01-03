0a41282d652740fe5c784722b22733ab71f3db70
Jan Steffen <jan.steffen@finleap.com>
2022-01-03 11:21:47 +0100

    Add auto reloader for tls conf
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

---
+++ pkg/tls/tls_config.go
@@ -0,0 +1,169 @@
+// Copyright 2022 Monoskope Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tls
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"path/filepath"
+	"sync"
+
+	"github.com/finleap-connect/monoskope/pkg/logger"
+	"github.com/pkg/errors"
+	"gopkg.in/fsnotify.v1"
+)
+
+type TLSConfigLoader struct {
+	mu         sync.RWMutex
+	caCertFile string
+	certFile   string
+	keyFile    string
+	keyPair    *tls.Certificate
+	rootCAs    *x509.CertPool
+	watcher    *fsnotify.Watcher
+	watching   chan struct{}
+	log        logger.Logger
+}
+
+func NewTLSConfigLoader(caCertFile, certFile, keyFile string) (*TLSConfigLoader, error) {
+	var err error
+	caCertFile, err = filepath.Abs(caCertFile)
+	if err != nil {
+		return nil, err
+	}
+	certFile, err = filepath.Abs(certFile)
+	if err != nil {
+		return nil, err
+	}
+	keyFile, err = filepath.Abs(keyFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TLSConfigLoader{
+		mu:         sync.RWMutex{},
+		caCertFile: caCertFile,
+		certFile:   certFile,
+		keyFile:    keyFile,
+		log:        logger.WithName("auto-reloading-tls-config").WithValues("caCertFile", caCertFile, "certFile", certFile, "keyFile", keyFile),
+	}, nil
+}
+
+// Watch starts watching for changes to the certificate
+// and key files. On any change the certificate and key
+// are reloaded. If there is an issue the load will fail
+// and the old (if any) certificates and keys will continue
+// to be used.
+func (t *TLSConfigLoader) Watch() error {
+	var err error
+	if t.watcher, err = fsnotify.NewWatcher(); err != nil {
+		return errors.Wrap(err, "can't create watcher")
+	}
+	if err = t.watcher.Add(t.caCertFile); err != nil {
+		return errors.Wrap(err, "can't watch ca cert file")
+	}
+	if err = t.watcher.Add(t.certFile); err != nil {
+		return errors.Wrap(err, "can't watch cert file")
+	}
+	if err = t.watcher.Add(t.keyFile); err != nil {
+		return errors.Wrap(err, "can't watch key file")
+	}
+	if err := t.load(); err != nil {
+		t.log.Error(err, "can't load")
+	}
+	t.log.Info("watching for ca, cert and key change")
+	t.watching = make(chan struct{})
+	go t.run()
+	return nil
+}
+
+func (t *TLSConfigLoader) load() error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	// Get the SystemCertPool, continue with an empty pool on error
+	rootCAs, _ := x509.SystemCertPool()
+	if rootCAs == nil {
+		rootCAs = x509.NewCertPool()
+	}
+
+	// Load local CA
+	certs, err := ioutil.ReadFile(t.caCertFile)
+	if err != nil {
+		return err
+	}
+
+	// Append local CA cert to the system pool
+	if ok := rootCAs.AppendCertsFromPEM(certs); !ok {
+		t.log.Info("No certs appended, using system certs only")
+	}
+
+	keyPair, err := tls.LoadX509KeyPair(t.certFile, t.keyFile)
+	if err == nil {
+		t.keyPair = &keyPair
+		t.log.Info("certificate and key loaded")
+	}
+
+	return err
+}
+
+func (t *TLSConfigLoader) run() {
+loop:
+	for {
+		select {
+		case <-t.watching:
+			break loop
+		case event := <-t.watcher.Events:
+			t.log.Info("watch event: %v", event)
+			if err := t.load(); err != nil {
+				t.log.Info("can't load: %v", err)
+			}
+		case err := <-t.watcher.Errors:
+			t.log.Info("error watching files: %v", err)
+		}
+	}
+	t.log.Info("stopped watching")
+	t.watcher.Close()
+}
+
+// GetTLSConfig returns a tls.Config with auto reloading certs.
+func (t *TLSConfigLoader) GetTLSConfig() *tls.Config {
+	return &tls.Config{
+		RootCAs:        t.GetRootCAs(),
+		GetCertificate: t.GetCertificate,
+	}
+}
+
+// GetRootCAs returns the cert pool to use to verify certificates.
+func (t *TLSConfigLoader) GetRootCAs() *x509.CertPool {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.rootCAs
+}
+
+// GetCertificate returns the loaded certificate for use by
+// the TLSConfig fields GetCertificate field in a http.Server.
+func (t *TLSConfigLoader) GetCertificate(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	return t.keyPair, nil
+}
+
+// Stop stops watching for changes to the
+// certificate and key files.
+func (t *TLSConfigLoader) Stop() {
+	t.watching <- struct{}{}
+}
