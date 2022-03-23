6f57f6dbd37b714d0a11517acd522a697de512da
Jan Steffen <jan.steffen@finleap.com>
2022-03-23 15:11:50 +0100

    Fix go test
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- opa.mk
+++ opa.mk
@@ -1,7 +1,7 @@
 
 ##@ OPA
 
-POLICIES_PATH = build/package/helm/gateway/files/policies
+export POLICIES_PATH = $(BUILD_PATH)/build/package/helm/gateway/files/policies
 
 opa-test: ## run all tests
 	@opa test -v $(POLICIES_PATH)/policies.rego $(POLICIES_PATH)/policies_test.rego
