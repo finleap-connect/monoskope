5a44663822f0456caec877bad1d9562fd5d761a0
Jan Steffen <jan.steffen@finleap.com>
2022-04-11 11:10:00 +0200

    Remove deprecated API
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- internal/gateway/gateway_server.go
+++ internal/gateway/gateway_server.go
@@ -48,54 +48,6 @@ func NewGatewayAPIServer(authConfig *auth.ClientConfig, client *auth.Client, ser
 	return s
 }
 
-func (s *gatewayApiServer) GetAuthInformation(ctx context.Context, state *api.AuthState) (*api.AuthInformation, error) {
-	url, encodedState, err := s.authClient.GetAuthCodeURL(state.CallbackUrl)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "invalid argument: %v", err)
-	}
-	return &api.AuthInformation{AuthCodeUrl: url, State: encodedState}, nil
-}
-
-func (s *gatewayApiServer) ExchangeAuthCode(ctx context.Context, code *api.AuthCode) (*api.AuthResponse, error) {
-	// Exchange auth code with upstream identity provider
-	s.log.Info("Exchanging auth code with issuer...")
-	upstreamClaims, err := s.authClient.Exchange(ctx, code.GetCode(), code.GetState())
-	if err != nil {
-		s.log.Error(err, "User authentication failed.")
-		return nil, err
-	}
-	s.log.Info("Exchanged successful, received upstream claims.", "name", upstreamClaims.Name, "email", upstreamClaims.Email)
-
-	// Check that a user exists in monoskope
-	s.log.Info("Checking user exists...", "email", upstreamClaims.Email)
-	user, err := s.userRepo.ByEmail(ctx, upstreamClaims.Email)
-	if err != nil {
-		return nil, err
-	}
-	s.log.Info("User exists!", "name", user.Name, "email", user.Email, "id", user.ID())
-
-	// Override upstream name
-	upstreamClaims.Name = user.Name
-
-	// Issue token
-	signedToken, rawToken, err := s.authServer.IssueToken(ctx, upstreamClaims, user.ID().String())
-	if err != nil {
-		s.log.Error(err, "Issuing token failed.")
-		return nil, err
-	}
-
-	// Create response
-	userInfo := &api.AuthResponse{
-		AccessToken: signedToken,
-		Expiry:      timestamppb.New(rawToken.Expiry.Time()),
-		Username:    user.Name,
-	}
-
-	s.log.Info("User authenticated successfully.", "User", upstreamClaims.Email, "Expiry", userInfo.Expiry.AsTime().String())
-
-	return userInfo, nil
-}
-
 func (s *gatewayApiServer) RequestUpstreamAuthentication(ctx context.Context, request *api.UpstreamAuthenticationRequest) (*api.UpstreamAuthenticationResponse, error) {
 	upstreamIDPUrl, encodedState, err := s.authClient.GetAuthCodeURL(request.GetCallbackUrl())
 	if err != nil {
