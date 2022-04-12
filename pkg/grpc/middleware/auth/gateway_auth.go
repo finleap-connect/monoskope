4d2def302f41fad338897eeddbb65d4cc36ea191
Jan Steffen <jan.steffen@finleap.com>
2022-04-12 15:58:24 +0200

    Add more logging
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- pkg/grpc/middleware/auth/gateway_auth.go
+++ pkg/grpc/middleware/auth/gateway_auth.go
@@ -20,6 +20,7 @@ import (
 
 	api "github.com/finleap-connect/monoskope/pkg/api/gateway"
 	"github.com/finleap-connect/monoskope/pkg/grpc/middleware"
+	"github.com/finleap-connect/monoskope/pkg/logger"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	"google.golang.org/grpc"
@@ -28,12 +29,14 @@ import (
 )
 
 type authMiddleware struct {
+	log                logger.Logger
 	gatewayClient      api.GatewayAuthClient
 	bypassAuthPrefixes []string
 }
 
 func NewAuthMiddleware(gatewayClient api.GatewayAuthClient, bypassAuthPrefixes []string) middleware.GRPCMiddleware {
 	return &authMiddleware{
+		logger.WithName("authMiddleware"),
 		gatewayClient,
 		bypassAuthPrefixes,
 	}
@@ -41,21 +44,28 @@ func NewAuthMiddleware(gatewayClient api.GatewayAuthClient, bypassAuthPrefixes [
 
 // authWithGateway calls the Gateway to authenticate the request and enriches the new context with tags set by the Gateway.
 func (m *authMiddleware) authWithGateway(ctx context.Context, fullMethodName string, req interface{}) (context.Context, error) {
+	m.log.V(logger.DebugLevel).Info("Authenticating request...", "fullMethodName", fullMethodName, "req", req)
+
 	// bypass auth for configured paths
+	m.log.V(logger.DebugLevel).Info("Checking for auth bypass...", "fullMethodName", fullMethodName, "req", req)
 	if m.bypassAuthPrefixes != nil {
 		for _, bypassAuthMethod := range m.bypassAuthPrefixes {
 			if strings.HasPrefix(fullMethodName, bypassAuthMethod) {
+				m.log.V(logger.DebugLevel).Info("Bypassing auth because of configured prefix.", "fullMethodName", fullMethodName, "req", req, "prefix", bypassAuthMethod)
 				return ctx, nil
 			}
 		}
 	}
+	m.log.V(logger.DebugLevel).Info("No matching bypass.", "fullMethodName", fullMethodName, "req", req)
 
 	// Check request is authenticated and authorized
+	m.log.V(logger.DebugLevel).Info("Authenticating request via gateway...", "fullMethodName", fullMethodName, "req", req)
 	response, err := m.gatewayClient.Check(ctx, &api.CheckRequest{
 		FullMethodName: fullMethodName,
 	})
 	if err != nil {
-		return nil, status.Errorf(codes.Unauthenticated, "gateway authN failed: %v", err)
+		m.log.V(logger.DebugLevel).Error(err, "gateway auth failed", "fullMethodName", fullMethodName, "req", req)
+		return nil, status.Errorf(codes.Unauthenticated, "gateway auth failed: %v", err)
 	}
 
 	// Add tags from response to context
@@ -70,6 +80,7 @@ func (m *authMiddleware) authWithGateway(ctx context.Context, fullMethodName str
 	newCtx := grpc_ctxtags.SetInContext(ctx, tags)
 
 	// Return new context with auth infos
+	m.log.V(logger.DebugLevel).Info("Authenticating successful!", "fullMethodName", fullMethodName, "req", req, "tags", tags)
 	return newCtx, nil
 }
 
