9757c357121b35a4a2b00b3d65f15751a075e5c5
Jan Steffen <jan.steffen@finleap.com>
2022-06-15 14:35:00 +0200

    Change to outgoing context
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- internal/scimserver/auth_handler.go
+++ internal/scimserver/auth_handler.go
@@ -15,7 +15,6 @@
 package scimserver
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/elimity-com/scim"
@@ -46,8 +45,8 @@ func (h *authHandler) withAuthContext(r *http.Request) (*http.Request, error) {
 			Detail: "Request unauthenticated with " + auth.AuthScheme,
 		}
 	}
-	md := metadata.Pairs(auth.HeaderAuthorization, fmt.Sprintf("%s %v", auth.AuthScheme, token))
-	nCtx := metautils.NiceMD(md).ToIncoming(r.Context())
+	md := metadata.Pairs(auth.HeaderAuthorization, auth.AuthScheme+" "+token)
+	nCtx := metautils.NiceMD(md).ToOutgoing(r.Context())
 	return r.WithContext(nCtx), nil
 }
 
