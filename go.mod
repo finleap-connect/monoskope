b3003e19c9aec370c4e64be208d7aa422a670392
Jan Steffen <jan.steffen@finleap.com>
2022-07-07 14:52:53 +0200

    Implement validation
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- go.mod
+++ go.mod
@@ -48,7 +48,10 @@ require (
 	sigs.k8s.io/controller-runtime v0.12.3
 )
 
-require k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9
+require (
+	github.com/go-git/go-git/v5 v5.4.2
+	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9
+)
 
 require (
 	cloud.google.com/go v0.99.0 // indirect
@@ -56,8 +59,10 @@ require (
 	github.com/Microsoft/go-winio v0.5.2 // indirect
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/OneOfOne/xxhash v1.2.8 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
 	github.com/agnivade/levenshtein v1.0.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -72,9 +77,12 @@ require (
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/emicklei/go-restful v2.9.5+incompatible // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.14 // indirect
@@ -91,10 +99,12 @@ require (
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
 	github.com/klauspost/compress v1.14.1 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.10.4 // indirect
@@ -103,6 +113,7 @@ require (
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/miekg/dns v1.1.47 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -129,6 +140,7 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
 	github.com/vmihailenco/tagparser v0.1.2 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	github.com/xanzy/ssh-agent v0.3.0 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
@@ -145,6 +157,7 @@ require (
 	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/api v0.24.2 // indirect
 	k8s.io/klog/v2 v2.60.1 // indirect
--- internal/k8sauthzreactor/config.go
+++ internal/k8sauthzreactor/config.go
@@ -15,49 +15,59 @@
 package k8sauthzreactor
 
 import (
+	"fmt"
+	"os"
 	"time"
 
 	es "github.com/finleap-connect/monoskope/pkg/eventsourcing"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 	"gopkg.in/yaml.v2"
+)
+
+var (
+	DefaultTimeout = 60 * time.Second
 )
 
 // GitRepoReconcilerConfig is the configuration for the GitRepoReconciler.
 type GitRepoReconcilerConfig struct {
-	Repositories []GitRepository      `yaml:"repositories"`
-	Mappings     []ClusterRoleMapping `yaml:"mappings"`
+	Repositories []*GitRepository      `yaml:"repositories"`
+	Mappings     []*ClusterRoleMapping `yaml:"mappings"`
+	cloneOptions *git.CloneOptions
 }
 
-// GitCredentialsBasicAccessAuth is used to authenticate towards a Git repository over HTTPS using basic access authentication.
-type GitCredentialsBasicAccessAuth struct {
+// GitBasicAuth is used to authenticate towards a Git repository over HTTPS using basic access authentication.
+type GitBasicAuth struct {
 	Username string `yaml:"username"`
 	Password string `yaml:"password"`
 }
 
 // GitSSHAuth is used to authenticate towards a Git repository over SSH. With the respective private key of the SSH key pair, and the host keys of the Git repository.
 type GitSSHAuth struct {
-	Identity   string `yaml:"identity"`
-	KnownHosts string `yaml:"knownHosts"`
+	PrivateKeyPath string `yaml:"privateKeyPath"`
+	Password       string `yaml:"password"`
 }
 
 // GitRepository is configuration to connect to a git repository.
 type GitRepository struct {
 	// URL is a required field that specifies the HTTP/S or SSH address of the Git repository.
 	URL string `yaml:"url"`
-	// CA is the Certificate Authority to trust while connecting with a Git repository over HTTPS.
+	// CA is an optional field to specify the Certificate Authority to trust while connecting with a git repository over HTTPS. If not specified OS CA's are used.
 	CA string `yaml:"caCert"`
-	// Branch is the branch of the repository to use.
+	// Branch is a required field that specifies the branch of the repository to use.
 	Branch string `yaml:"branch"`
 	// Internal is a required field that specifies the interval at which the Git repository must be fetched.
-	Interval time.Duration `yaml:"interval"`
-	// Timeout is an optional field to specify a timeout for Git operations like cloning.
-	Timeout time.Duration `yaml:"timeout"`
-	// allClusters specifies if the RBAC for all clusters should be managed.
+	Interval *time.Duration `yaml:"interval"`
+	// Timeout is an optional field to specify a timeout for Git operations like cloning. Defaults to 60s.
+	Timeout *time.Duration `yaml:"timeout"`
+	// AllClusters is an optional field to specify if the RBAC for all clusters should be managed. Defaults to false.
 	AllClusters bool `yaml:"allClusters"`
-	// Clusters specifies a list of clusters for which the RBAC should be managed.
+	// Clusters is an optional field to specify a list of clusters for which the RBAC should be managed.
 	Clusters []string `yaml:"clusters"`
-	// CredentialsBasicAccessAuth is used to authenticate towards a Git repository over HTTPS using basic access authentication.
-	CredentialsBasicAccessAuth *GitCredentialsBasicAccessAuth `yaml:"credentialsBasicAccessAuth"`
-	// SSHAuth is used to authenticate towards a Git repository over SSH. With the respective private key of the SSH key pair, and the host keys of the Git repository.
+	// BasicAuth is an optional field to specify credentials to authenticate towards a Git repository over HTTPS using basic access authentication.
+	BasicAuth *GitBasicAuth `yaml:"basicAuth"`
+	// SSHAuth is an optional field to specify credentials to authenticate towards a Git repository over SSH. With the respective private key of the SSH key pair, and the host keys of the Git repository.
 	SSHAuth *GitSSHAuth `yaml:"sshAuth"`
 }
 
@@ -68,13 +78,65 @@ type ClusterRoleMapping struct {
 	ClusterRoleName string   `yaml:"clusterRoleName"`
 }
 
-// NewConfigFromFile creates a new Config from a given yaml file
+// NewConfigFromFile creates a new GitRepoReconcilerConfig from a given yaml file
 func NewConfigFromFile(data []byte) (*GitRepoReconcilerConfig, error) {
+	// Unmarshal
 	conf := &GitRepoReconcilerConfig{}
 	err := yaml.Unmarshal(data, conf)
 	if err != nil {
 		return nil, err
 	}
 
+	for _, repo := range conf.Repositories {
+		// Check required fields are set
+		if err := conf.parseCloneOptions(repo); err != nil {
+			return conf, err
+		}
+
+		if repo.Interval == nil {
+			return nil, ErrIntervalIsRequired
+		}
+
+		// Set default values
+		if repo.Timeout == nil {
+			repo.Timeout = &DefaultTimeout
+		}
+	}
+
 	return conf, nil
+}
+
+// parseCloneOptions parses the configuration using the git library to validate.
+func (c *GitRepoReconcilerConfig) parseCloneOptions(repo *GitRepository) error {
+	cloneOptions := &git.CloneOptions{
+		URL: repo.URL,
+	}
+
+	// Configure basic auth optionally
+	if repo.BasicAuth != nil {
+		cloneOptions.Auth = &http.BasicAuth{
+			Username: repo.BasicAuth.Username,
+			Password: repo.BasicAuth.Password,
+		}
+	}
+
+	// Configure ssh auth
+	if repo.SSHAuth != nil {
+		_, err := os.Stat(repo.SSHAuth.PrivateKeyPath)
+		if err != nil {
+			return fmt.Errorf("read file %s failed: %w", repo.SSHAuth.PrivateKeyPath, err)
+		}
+
+		publicKeys, err := ssh.NewPublicKeysFromFile("git", repo.SSHAuth.PrivateKeyPath, repo.SSHAuth.Password)
+		if err != nil {
+			return err
+		}
+		cloneOptions.Auth = publicKeys
+	}
+
+	if err := cloneOptions.Validate(); err != nil {
+		return err
+	}
+	c.cloneOptions = cloneOptions
+	return nil
 }
