1513e7745f79dfad231b14c0c6944c98f5f4b775
Jan Steffen <jan.steffen@finleap.com>
2022-09-28 16:06:37 +0200

    make it configurable via env
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- internal/tracing/tracing.go
+++ internal/tracing/tracing.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Monoskope Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package tracing
 
 import (
@@ -13,20 +27,35 @@ import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
-// InitOpenTelemetry creates a new MeterProvider and TracerProvider for OpenTelemetry from env vars
+/*
+Find detailed documentation at https://pkg.go.dev/go.opentelemetry.io/otel/exporters/otlp/otlptrace#readme-examples.
+*/
+
+// InitOpenTelemetry configures and sets the global MeterProvider and TracerProvider for OpenTelemetry
 func InitOpenTelemetry(ctx context.Context) (func() error, error) {
-	res, err := resource.New(ctx,
-		resource.WithFromEnv(), // pull attributes from OTEL_RESOURCE_ATTRIBUTES and OTEL_SERVICE_NAME environment variables
-		resource.WithProcess(),
-		resource.WithHost(),
-		resource.WithOS(),
-		resource.WithContainer(),
-		resource.WithContainerID(),
-	)
+	meterProviderShutdown, err := InitMeterProvider(ctx)
 	if err != nil {
 		return nil, err
 	}
 
+	tracerProviderShutdown, err := InitTracerProvider(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return func() error {
+		if err := meterProviderShutdown(); err != nil {
+			return err
+		}
+		if err := tracerProviderShutdown(); err != nil {
+			return err
+		}
+		return nil
+	}, nil
+}
+
+// InitMeterProvider configures and sets the global MeterProvider
+func InitMeterProvider(ctx context.Context) (func() error, error) {
 	meterExporter, err := otlpmetricgrpc.New(ctx)
 	if err != nil {
 		panic(err)
@@ -35,25 +64,31 @@ func InitOpenTelemetry(ctx context.Context) (func() error, error) {
 	meterProvider := metric.NewMeterProvider(metric.WithReader(metric.NewPeriodicReader(meterExporter)))
 	global.SetMeterProvider(meterProvider)
 
+	return func() error { return meterProvider.Shutdown(ctx) }, nil
+}
+
+// InitTracerProvider configures and sets the global TracerProvider
+func InitTracerProvider(ctx context.Context) (func() error, error) {
 	client := otlptracegrpc.NewClient()
 	traceExporter, err := otlptrace.New(ctx, client)
 	if err != nil {
 		return nil, err
 	}
 
+	res, err := resource.New(ctx,
+		resource.WithFromEnv(),
+		resource.WithHost(),
+		resource.WithOS(),
+		resource.WithContainer(),
+	)
+	if err != nil {
+		return nil, err
+	}
 	tracerProvider := sdktrace.NewTracerProvider(
 		sdktrace.WithBatcher(traceExporter),
 		sdktrace.WithResource(res),
 	)
 	otel.SetTracerProvider(tracerProvider)
 
-	return func() error {
-		if err := meterProvider.Shutdown(ctx); err != nil {
-			return err
-		}
-		if err := tracerProvider.Shutdown(ctx); err != nil {
-			return err
-		}
-		return nil
-	}, nil
+	return func() error { return tracerProvider.Shutdown(ctx) }, nil
 }
